86ab70a1760f5ab55eede0f1ca90880f1fa9c9f7
Boris Périsset <sehroiber@gmail.com>
2021-10-15 11:38:52 +0200

    Score zählt hoch und runter

--- pong.js
+++ pong.js
@@ -3,7 +3,6 @@ let puck = []
 
 
 
-
 function setup() {
   var width = 600;
   var height = 500;
@@ -11,14 +10,15 @@ function setup() {
   var canvas =   createCanvas(width, height);
   // Move the canvas so it’s inside our <div id="sketch-holder">.
   canvas.parent('pong-frame');
-
-
-  // das Object «Ball» mit seinen Eigenschaften. Ball = Puck und viceversa...
   ellipseMode(RADIUS);
 
+
+
+  // das Object und Array «Ball» mit seinen Eigenschaften. Ball = Puck und viceversa...
+
   let Ball =  {
-    xStart: width/2,
-    yStart: height/2,
+    xPos: width/2,
+    yPos: height/2,
     speedX: random(-2,5), //entscheidet sich randommässig für eine Geschwindigkeit
     speedY: random(-2,5), //entscheidet sich randommässig für eine Geschwindigkeit
     radius: 10
@@ -28,6 +28,11 @@ function setup() {
 
 }
 
+  // das Object Paddle 01, damit wir die Paddle Position herausfinden können.
+  let pad =  {
+    padHeight: 90,
+    paddlePosY: [] //Dies sollte die untere yPos Ecke des Paddles tracken...
+  }
 
 function draw() {
 
@@ -42,33 +47,47 @@ function draw() {
   text("Score: " + score, 12,  25);
  
   //Paddle 01
-  rect(10, mouseY, 10, 90);
+  rect(10, mouseY, 10, pad.padHeight);
 
   // Paddle 02 (macht noch das gleiche wie Paddle 01)
   rect(width-20, mouseY, 10, 90);
 
 
+
+
   // Ball oder Puck bounced
   for (let i = 0; i < puck.length; i += 1) {
     
-  // Variable mit einzelnem Element aus dem Array 
+  // Variable mit einzelnem Element aus dem Array Ball
   let currentPos = puck[i];
 
   //Ball zeichnen (aus dem Array und dem Object Ball sowie der Current Position)
-  ellipse(currentPos.xStart, currentPos.yStart, currentPos.radius);
+  ellipse(currentPos.xPos, currentPos.yPos, currentPos.radius);
   
   // Ball prallt ab (Bounce)
-  if (currentPos.xStart > (width-currentPos.radius) || currentPos.xStart < (0+currentPos.radius)) {
+  if (currentPos.xPos > (width-currentPos.radius) || currentPos.xPos < (0+currentPos.radius)) {
     currentPos.speedX = currentPos.speedX  * -1;
   }
 
-  if (currentPos.yStart > (height-currentPos.radius) || currentPos.yStart < (0+currentPos.radius)) {
+  if (currentPos.yPos > (height-currentPos.radius) || currentPos.yPos < (0+currentPos.radius)) {
     currentPos.speedY = currentPos.speedY * -1;
   }
-
   // Ball fliegt (langsam)
-  currentPos.xStart += currentPos.speedX;
-  currentPos.yStart += currentPos.speedY;
+  currentPos.xPos += currentPos.speedX;
+  currentPos.yPos += currentPos.speedY;
+
+
+  // Score zählt hoch und runter
+  if (currentPos.xPos <= 10) {
+    score += -1;
+  }
+
+  if (currentPos.xPos >= width-10) {
+    score += 1;
+  }
+
 
 }
+
+
 }
