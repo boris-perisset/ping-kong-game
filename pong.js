0370ed0de2201515c980fe30a91ff77b237c3864
Boris Périsset <sehroiber@gmail.com>
2021-10-15 11:58:11 +0200

    Score zählt hoch und Ball wird neu gesetzt.

--- pong.js
+++ pong.js
@@ -3,10 +3,12 @@ let puck = []
 
 
 
+
 function setup() {
+
+  
   var width = 600;
   var height = 500;
-
   var canvas =   createCanvas(width, height);
   // Move the canvas so it’s inside our <div id="sketch-holder">.
   canvas.parent('pong-frame');
@@ -19,8 +21,8 @@ function setup() {
   let Ball =  {
     xPos: width/2,
     yPos: height/2,
-    speedX: random(-2,5), //entscheidet sich randommässig für eine Geschwindigkeit
-    speedY: random(-2,5), //entscheidet sich randommässig für eine Geschwindigkeit
+    speedX: random(-5,5), //entscheidet sich randommässig für eine Geschwindigkeit und Richtung
+    speedY: random(-5,5), //entscheidet sich randommässig für eine Geschwindigkeit und Richtung
     radius: 10
   }
   // erstellt einen Array mit um die currentPosition danach bestimmen zu können.
@@ -44,7 +46,7 @@ function draw() {
   textSize(24);
 
   textFont('Press Start 2P');
-  text("Score: " + score, 12,  25);
+  text("Your Score: " + score, 12,  25);
  
   //Paddle 01
   rect(10, mouseY, 10, pad.padHeight);
@@ -77,13 +79,26 @@ function draw() {
   currentPos.yPos += currentPos.speedY;
 
 
-  // Score zählt hoch und runter
+  
+  // Score zählt hoch und runter (Die 10 zählt ist der Radius des Balles, damits funktioniert.)
+  // Ball wird in die Mitte zurück gesetzt und zufällig in eine neue Richtung geschickt.
   if (currentPos.xPos <= 10) {
     score += -1;
+    currentPos.speedX = random(-5,5);
+    currentPos.speedY = random(-5,5);
+    currentPos.xPos = width/2;
+
+
   }
 
   if (currentPos.xPos >= width-10) {
     score += 1;
+    currentPos.speedX = random(-5,5);
+    currentPos.speedY = random(-5,5);
+    currentPos.xPos = width/2;
+
+
+
   }
 
 
