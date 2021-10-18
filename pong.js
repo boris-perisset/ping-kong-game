fda98347ee979de3df7076bccb374609567802e0
Boris Périsset <sehroiber@gmail.com>
2021-10-18 16:00:53 +0200

    Sauberes Spiel (ein wenig besser)

--- pong.js
+++ pong.js
@@ -1,7 +1,7 @@
 let score1 = 0
 let score2 = 0
 let puck = []
-let gameSet = 1
+let gameSet = 5
 
 let state = {
   start: true,
@@ -9,8 +9,21 @@ let state = {
   over: false
 }
 
-let button;
+let pad =  {
+  padHeight: 90,
+  paddle02_Y: 0
+}
 
+  /*
+  ===============================================================================
+  =====================   Event Handlers    ==========================================
+  ===============================================================================
+  */
+
+function mouseClicked() {
+  state.game = true;
+  state.start = false;
+}
 
   /*
   ===============================================================================
@@ -38,14 +51,9 @@ function setup() {
   }
   // erstellt einen Array mit um die currentPosition danach bestimmen zu können.
   puck.push(Ball)
+
 }
   // das Object Paddle 01, damit wir die Paddle Position herausfinden können. Vielleicht...
-  let pad =  {
-    padHeight: 90,
-    paddle02_Y: 0
-  }
-
- 
 
   /*
   ===============================================================================
@@ -53,57 +61,81 @@ function setup() {
   ===============================================================================
   */
 
+
 function draw() {
 
   textSize(24);
   fill (0, 255, 67);
   textAlign(CENTER, CENTER);
   textFont('Press Start 2P');
+  cursor(ARROW)
 
   /* =====================     GAME START      ====================================== */
   
   if (state.start == true) {
+    background (0);
+    fill (0, 255, 67);
+    text("Click to Play", width/2, height/2);
+    cursor(HAND);
 
-    text("Start Game", width/2, height/2);
-    
-    button = createButton('Play Game');
-    button.position(width/2, width/2);
-    button.mousePressed(startGame);
-  
-    function startGame() {
-      state.game = true;
+
+
+  /*
+    function startText () {
+      this.title = text("Click to Play", width/2, height/2),
+      this.hover = cursor(HAND),
+      this.action =  function mouseClicked() {
+        state.game = true;
+        state.start = false;
+      }
     }
-
+    startText ();
+    console.log ("Start", state.start, "Game", state.game)
+    */
   }
+
 
   /* =====================     GAME OVER      ====================================== */
 
   if (state.over == true) {
+    background (0);
+    fill (0, 255, 67);
+
+    state.game = false;
+    state.start = false;
+
     text("Game Over", width/2, height/2);
     
     let result = "Whatever";
 
+    if (score1 == gameSet) {
+      let result = "You Win";
+      }
+      
     if (score2 == gameSet) {
       let result = "You Loose";
-      } else {
-      result = "You Win";
-    }
+      }
 
-    let looserMessages = [result, "3", "2", "1"];
+    let finalMessages = [result, "3", "2", "1"];
 
-    for (let message of looserMessages) {
+    for (let message of finalMessages) {
       frameRate (1);
+
+      background (0);
+      fill (0, 255, 67);
       text(message, width/2, height/2);
       console.log(message)
     }
+    cursor(HAND);
+    background (0);
+    text("Play Again?", width/2, height/2);
 
     score1 = 0;
     score2 = 0;
 
-    state.over = false;
     state.game = false;
+    state.over = false;
     state.start = true;
-  
     frameRate(60);
   } 
   
@@ -112,6 +144,8 @@ function draw() {
 
 
   if (state.game == true) {
+    state.over = false;
+    state.start = false;
     // game running
     // Background
     background(0);
