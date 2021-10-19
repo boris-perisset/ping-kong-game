3b18cc773abfae9acce0746d86545e5c05f533fc
Boris Périsset <sehroiber@gmail.com>
2021-10-19 13:52:41 +0200

    Update nach Davids Review

--- pong.js
+++ pong.js
@@ -1,29 +1,49 @@
-let score1 = 0
-let score2 = 0
-let puck = []
-let gameSet = 5
+let score1 = 0;
+let score2 = 0;
 
-let state = {
-  start: true,
-  game: false,
-  over: false
+const puck = [];
+const gameSet = 5;
+
+
+let currentGameState = 'start';
+if (currentGameState === 'over') {
 }
 
-let pad =  {
+const pad = {
   padHeight: 90,
   paddle02_Y: 0
 }
 
-  /*
-  ===============================================================================
-  =====================   Event Handlers    ==========================================
-  ===============================================================================
-  */
 
-function mouseClicked() {
-  state.game = true;
-  state.start = false;
+function getRandomXSpeed () {
+  let speedX = random(-6, -3);
+
+  if (random() < .5) {
+    speedX = random(3, 6);
+  }
+
+  return speedX;
 }
+
+
+function getRandomYSpeed () {
+  let speedY = random(-6, -1);
+
+  if (random() < .5) {
+    speedY = random(1, 6);
+  }
+
+  return speedY;
+}
+
+
+/* SOUND VARIABLES
+var ping
+var Pong
+var loose
+var win
+*/
+
 
   /*
   ===============================================================================
@@ -31,27 +51,48 @@ function mouseClicked() {
   ===============================================================================
   */
 
-function setup() {
-  var width = 600;
-  var height = 500;
+  /* FUCK SOUND!!!
+function preload(){
+  ping = loadSound('sound/ping-01.mp3'+);
+  pong = loadSound('sound/pong-01.mp3');
+  loose = loadSound('sound/pong-loose.mp3');
+  win = loadSound('sound/pong-win.mp3');
+}
+*/
 
-  var canvas =   createCanvas(width, height);
+
+function setup() {
+  
+  const width = 600;
+  const height = 500;
+
+  /*
+  getAudioContext().suspend();
+
+  let sounds = new p5.MonoSynth();
+
+  ping.play('A6');
+  */
+
+  var canvas = createCanvas(width, height);
   // Move the canvas so it’s inside our <div id="sketch-holder">.
   canvas.parent('pong-frame');
   ellipseMode(RADIUS);
 
   // das Object und Array «Ball» mit seinen Eigenschaften. Ball = Puck und viceversa...
 
-  let Ball =  {
+  
+
+  const Ball = {
     xPos: width/2,
-    yPos: height/2,
-    speedX: random(-5,5), //entscheidet sich randommässig für eine Geschwindigkeit und Richtung
-    speedY: random(-5,5), //entscheidet sich randommässig für eine Geschwindigkeit und Richtung
+    yPos: (height * 0.2) + random(height * 0.6), // irgendwo in den mittleren 60% der höhe
+    speedX: random(-6, 6), //entscheidet sich randommässig für eine Geschwindigkeit und Richtung
+    speedY: getRandomYSpeed(), //entscheidet sich randommässig für eine Geschwindigkeit und Richtung
     radius: 10
   }
+
   // erstellt einen Array mit um die currentPosition danach bestimmen zu können.
   puck.push(Ball)
-
 }
   // das Object Paddle 01, damit wir die Paddle Position herausfinden können. Vielleicht...
 
@@ -61,96 +102,79 @@ function setup() {
   ===============================================================================
   */
 
-
-function draw() {
+function draw () {
 
   textSize(24);
-  fill (0, 255, 67);
+  fill(0, 255, 67);
   textAlign(CENTER, CENTER);
-  textFont('Press Start 2P');
-  cursor(ARROW)
+  textFont('VT323');
+  cursor(ARROW);
 
   /* =====================     GAME START      ====================================== */
   
-  if (state.start == true) {
-    background (0);
-    fill (0, 255, 67);
-    text("Click to Play", width/2, height/2);
-    cursor(HAND);
+  if (currentGameState === 'start') {
 
-
-
-  /*
-    function startText () {
-      this.title = text("Click to Play", width/2, height/2),
-      this.hover = cursor(HAND),
-      this.action =  function mouseClicked() {
-        state.game = true;
-        state.start = false;
-      }
+    background(0);
+    fill(0, 255, 67);
+    text("Press any key to Play", width/2, height/2);
+    
+    if (keyIsPressed === true) {
+      currentGameState = 'game';
     }
-    startText ();
-    console.log ("Start", state.start, "Game", state.game)
-    */
+
   }
 
 
   /* =====================     GAME OVER      ====================================== */
 
-  if (state.over == true) {
-    background (0);
-    fill (0, 255, 67);
-
-    state.game = false;
-    state.start = false;
-
-    text("Game Over", width/2, height/2);
-    
+  else if (currentGameState === 'over') {
+    frameRate(0.4);
     let result = "Whatever";
 
-    if (score1 == gameSet) {
-      let result = "You Win";
-      }
-      
-    if (score2 == gameSet) {
-      let result = "You Loose";
-      }
-
-    let finalMessages = [result, "3", "2", "1"];
-
-    for (let message of finalMessages) {
-      frameRate (1);
-
-      background (0);
-      fill (0, 255, 67);
-      text(message, width/2, height/2);
-      console.log(message)
+    if (score1 === gameSet) {
+      result = "You Win";
     }
-    cursor(HAND);
-    background (0);
+      
+    else if (score2 === gameSet) {
+      result = "You Loose";
+    }
+
+    textSize(64);
+    text(result, width/2, height/2);
+
+    currentGameState = 'again';
+    
+  } 
+  
+  /* =====================     PLAY AGAIN      ====================================== */
+
+  else if (currentGameState === 'again') {
+    
+    frameRate(1);
+    background(0);
+    fill(0, 255, 67);
+
     text("Play Again?", width/2, height/2);
+    text("Press any key", width/2, (height/2-(-30)));
 
     score1 = 0;
     score2 = 0;
 
-    state.game = false;
-    state.over = false;
-    state.start = true;
     frameRate(60);
-  } 
-  
+
+    if (keyIsPressed === true) {
+      currentGameState = 'game';
+    }
+  }
 
    /* =====================     GAME PLAY      ====================================== */
 
+  else if (currentGameState === 'game') {
 
-  if (state.game == true) {
-    state.over = false;
-    state.start = false;
-    // game running
     // Background
     background(0);
     fill (0, 255, 67);
-
+    
     // Texte als Objekte definiert, damit man es evtl später nutzen kann...
     let You = {
       Name: text("You: " + score1, 60,  30)
@@ -159,7 +183,7 @@ function draw() {
     You.Name;
 
     let Allan = {
-      Name:   text("Allan: " + score2, width-80,  30)
+      Name: text("Allan: " + score2, width-80,  30)
     }
     
     Allan.Name;
@@ -167,12 +191,12 @@ function draw() {
     //Paddle 01
     rect(10, mouseY, 10, pad.padHeight);
 
-    // Paddle 02 reagiert auf den Ball
+    // Paddle 02
     rect(width-20, pad.paddle02_Y, 10, 90);
 
     /* =====================     BALL      ====================================== */
 
-    // Ball bounced
+    // Ball bewegt sich
     for (let i = 0; i < puck.length; i += 1) {
         
       // Variable mit einzelnem Element aus dem Array Ball
@@ -203,15 +227,17 @@ function draw() {
 
       if((currentPos.xPos <= 30) && (currentPos.yPos >= mouseY && currentPos.yPos <= (mouseY + pad.padHeight))) {
         currentPos.speedX = currentPos.speedX  * -1;
-        //currentPos.speedY += currentPos.speedY + random(-0.01,0.01)
         currentPos.speedX += 0.5;
+        //ping.play();
+
       }
 
       // Paddle 02 Bounce
       if((currentPos.xPos >= width-30) && (currentPos.yPos >= pad.paddle02_Y && currentPos.yPos <= (pad.paddle02_Y + pad.padHeight))) {
         currentPos.speedX = currentPos.speedX  * -1;
-        //currentPos.speedY += currentPos.speedY + random(-0.01,0.01)
         currentPos.speedX += -0.5;
+        //pong.play();
+
       }
 
       /* =====================     SCORE 1UP      ====================================== */
@@ -221,39 +247,36 @@ function draw() {
       if (currentPos.xPos <= 10) {
         score2 += 1;
         currentPos.xPos = width/2;
-        currentPos.speedX = random(-5,5);
-        currentPos.speedY = random(-5,5);
-
+        currentPos.speedX = getRandomXSpeed();
+        currentPos.speedY = getRandomYSpeed();
       }
 
-      console.log ("Speed X:", currentPos.speedX);
+      //console.log ("Speed X:", currentPos.speedX);
 
       if (currentPos.xPos >= width-10) {
         score1 += 1;
         currentPos.xPos = width/2;
-        currentPos.speedX = random(-5,5);
-        currentPos.speedY = random(-5,5);
+        currentPos.speedX = getRandomXSpeed();
+        currentPos.speedY = getRandomYSpeed();
       }
 
+      /* =====================     PADDLE 2 - ENEMY      ====================================== */
 
-      /* =====================     ENEMY      ====================================== */
+      //Allan Alcorn inventor of Pong
 
-      // Paddle 02 The ENEMY!!! Allan Alcorn invetor of Pong
       pad.paddle02_Y = currentPos.yPos;
 
       if (currentPos.xPos > 560) {
-        let zufall = random(-3,6)
+        let zufall = random(-3,4)
         pad.paddle02_Y = currentPos.yPos + zufall;
-        //console.log ("Paddle 02 yPos", pad.paddle02_Y);
       }
 
 
       /* =====================     GAME OVER ?      ====================================== */
 
       // Game Over > hast du gewonnen oder verloren?
-      if (score1 || score2 == gameSet) {
-        frameRate(1);
-        state.over = true;
+      if ( score1 === gameSet || score2 === gameSet) {
+        currentGameState = 'over';
       }
 
     }
